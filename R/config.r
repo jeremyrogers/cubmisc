cbd11a34cbe78f31635d26a03517f47c59776a96
Michael Gilchrist <mikeg@utk.edu>
2014-07-15 16:45:24 -0400

    Added and revised comments to parameters in config.r

--- R/config.r
+++ R/config.r
@@ -38,22 +38,29 @@ print.config <- function(config)
 
 
 config <- list(
-  n.iter  = 5000,  # steps after burn.in between convergence checks.
-  use.n.iter = 1000, #window for convergence test (multichain) and window to calculate mean phi. IMPORTANT: Make sure this one is smaller than the thined chain, otherwise saving will crash!
+  n.iter  = 5000,  # number of iterations between convergence checks.
+  use.n.iter = 1000, #sample size for testing for convergence. 
+                     #If convergence threshold, set in eps, is reached the sample size of our posteriors equals this value. 
+		     #IMPORTANT: Make sure this one is smaller than the thined chain, otherwise saving will crash!
   n.chains = 4, # num chains
   n.cores = 4, # total num of cpu cores (should be about 5*n.chains when using parallel method other then "lapply")
   selected.env = 1, # deprecated (us if more than one dataset is stored in csv, e.g. different conditions)
   min.iter=200000, # minimum steps each chain has to do (without thining), convergence criterium is ignored until min.iter is reached 
-  max.iter=220000, # maximum steps for each chain (without thining). MCMC will be stoped if the chain reached max.iter iterations (convergence criterium is ignored)
+  max.iter=300000, # maximum steps for each chain (without thining). MCMC will be stoped if the chain reached max.iter iterations (convergence criterium is ignored)
   reset.qr=400, # stop resetting qr matrix when checking for convergence after this iteration (after thining)
   conv.thin=1,  # thining for convergence test (recommend 1 if chain.thin != 1, otherwise double thining)
-  chain.thin=50, # thining of the chain during runtime
+  chain.thin=50, # thining of the chain during runtime. This is done before gathering convergence test sample. See note for conv.thin.
   rm.first.aa=0, # remove first rm.first.aa AAs (after the first codon which is expected to be the start codon)
   rm.short=0, # # ignore sequences with length < rm.short AAs after the first rm.first.aa AAs are removed
-  parallel="lapply", # parallel method within chain (lapply = no parallelization within chain)
-  eps=0.05, # I usually use 0.05 when n.chain > 1 and XXXX otherwise
-  frac1=0.1, # % of chain at the beginning of the chain (use.n.iter window) used to calculate mean for convergence test
-  frac2=0.5, # % of chain at the end of the chain (use.n.iter window) used to calculate mean for convergence test
+  parallel="lapply", # parallel method within chain 
+                     # lapply = no parallelization within chain)
+                     # mclapply = parallelization within chain.
+		     # Other options are also possible.
+  eps=0.1, # Convergence threshold.
+            # Multichain MCMC uses with Gelman test where threshold is |Gelman Score - 1| < eps 
+            # Single chain MCMC uses Geweke test where Geweke score < eps.
+  frac1=0.1, # Used with single chain MCMC. Part of Geweke test. Value is proportion of chain at the beginning of the chain (use.n.iter window) used to calculate mean for convergence test
+  frac2=0.5, # Used with single chain MCMC. Part of Geweke test. Value is proportion of chain at the end of the chain (use.n.iter window) used to calculate mean for convergence test
   
   #aa = c("D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "W", "Y", "Z"),
   #aa = c("A", "V"), 
