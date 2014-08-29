85dc198fecb20bc9f9f358b4e4a9544c580993a7
ozway <lbrown60@vols.utk.edu>
2014-08-29 17:24:17 -0400

    test commit

--- R/workflow.sh
+++ R/workflow.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 
-cubmethod="cubappr" # change method
+cubmethod="cubfits" # change method
 sdlog="0.5 1 2 4" # change sdlog in command line, have to figure out why it is not working with variable
 genome="../data/ecoli_K12_MG1655_genome_filtered.fasta"
 empphi="../data/ecoli_X_obs.csv"
-prefix="without_xobs_" # change name
+prefix="with_xobs_" # change name
 suffix="multichain_lapply_4cores"
 folder="../results/conv_test/" # change folder
-pinit="woxobs_pinit.csv" # file with initial values for multiple parameters such as Ax, s_phi^{(0)}, s_\varepsilon
+pinit="wxobs_pinit.csv" # file with initial values for multiple parameters such as Ax, s_phi^{(0)}, s_\varepsilon
                 ##NOTE: With and without Xobs fits use different files.
 
 if [ ! -d "$folder" ]; then
@@ -20,7 +20,8 @@ logfile="$folder$prefix$suffix.log"
 
 if [ "$cubmethod" = "cubfits" ]; then
 	echo "running with x obs run\n"
-	nohup Rscript run_roc.r -c $cubmethod -s "0.5 1 2 4" -f $genome -p $empphi -o $folder -n $foutname -i $pinit >> $logfile &
+	#nohup Rscript run_roc.r -c $cubmethod -s "0.5 1 2 4" -f $genome -p $empphi -o $folder -n $foutname -i $pinit >> $logfile &
+	Rscript run_roc.r -c $cubmethod -s "0.5 1 2 4" -f $genome -p $empphi -o $folder -n $foutname -i $pinit >> $logfile
 fi
 if [ "$cubmethod" = "cubappr" ]; then
 	if [ "$sdlog" = "0" ]; then #double check that sdlog value is properly defined
